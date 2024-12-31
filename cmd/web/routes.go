6ab38c93499a7001dce8be5a6fd8151d9bd57bc5
Udeek Singh <udeeksingh@Udeeks-MacBook-Air.local>
2024-12-31 12:39:17 +0530

    updating html source files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,12 +17,16 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-	mux.Use(WriteToConsole)
+	// mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// adding static file path
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 	return mux
 
 }
