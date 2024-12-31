4eb5887eb4cd83313d882dccaa8ed80ad36238ff
Udeek Singh <udeeksingh@Udeeks-MacBook-Air.local>
2024-12-31 19:34:28 +0530

    updating all source code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	// adding static file path
 	fileServer := http.FileServer(http.Dir("./static/"))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -46,3 +46,28 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// render the Reservation page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// render the fenerals page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// render the majors page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// render the availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// render the Contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
